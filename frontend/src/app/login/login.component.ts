60476d211a76fae0eb31759acec410f6af85c775
zaid03 <terguyzaid@gmail.com>
2025-10-04 22:38:56 +0100

    -login / sign up static page is ready

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
 
-  
+  password: boolean = false;
+  showPassword: boolean = false;
+  showSignUp: boolean = false;
+
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleSignup(): void {
+    this.showSignUp = !this.showSignUp;
+  }
 
   blobPAth = 'assets/blob.svg';
   googlePath = 'assets/google.svg'
