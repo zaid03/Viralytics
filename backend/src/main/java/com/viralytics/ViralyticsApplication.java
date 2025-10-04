a5f5ffab63c7e1d4e87f8eed3673abdf0b8f1f7b
zaid03 <terguyzaid@gmail.com>
2025-10-04 15:03:45 +0100

    -set up frameworks to work with angular + spring boot

---
+++ backend/src/main/java/com/viralytics/ViralyticsApplication.java
@@ -0,0 +1,13 @@
+package com.viralytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViralyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViralyticsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/viralytics/ViralyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viralytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViralyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
