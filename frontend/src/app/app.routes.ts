d2b480d471bdedd7501ccf0649bc26c269badeb4
zaid03 <terguyzaid@gmail.com>
2025-10-05 16:53:37 +0100

    -new component dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { RouterModule ,Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 export const routes: Routes = [
+    { path: 'dashboard', component: DashboardComponent},
     { path: 'login', component: LoginComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full'}, //default route
 ];
