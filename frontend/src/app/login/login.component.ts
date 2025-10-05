95834f999409ab550c7ea0e175215b5a99656031
zaid03 <terguyzaid@gmail.com>
2025-10-05 16:49:08 +0100

    - ability to login added

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,30 +1,58 @@
-import { Component, output, EventEmitter } from '@angular/core';
+import { Component} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule ],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
-  password: boolean = false;
+  blobPAth = 'assets/blob.svg';
+  googlePath = 'assets/google.svg'
   showPassword: boolean = false;
   showSignUp: boolean = false;
 
   togglePassword(): void {
     this.showPassword = !this.showPassword;
   }
-
   toggleSignup(): void {
     this.showSignUp = !this.showSignUp;
   }
 
-  blobPAth = 'assets/blob.svg';
-  googlePath = 'assets/google.svg'
+  name = '';
+  email = '';
+  password = '';
+  errorMessage: string | null = null;
+  isError: boolean = false;
+
+  submit(): void{
+    const payload = {
+      email: this.email,
+      password: this.password,
+    }
+
+    this.http.post<{ status?: string }>('http://localhost:8080/api/auth/login', payload
+    ).subscribe({
+      next: (res) => {
+        console.log("login succesfull.", res);
+        this.errorMessage = null;
+        this.isError = false;
+      },
+      error: (err) => {
+        this.errorMessage = err?.error?.error || err?.message || 'login failed';
+        this.isError = true;
+        console.log("login error", err);
+        console.log("miaw");
+      }
+    })
+  }
+
+  
 }
